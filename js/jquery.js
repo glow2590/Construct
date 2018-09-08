7a3c97eaf84b7f059e5c6800ab6137c04bfeaf20
diaa mohamed <diaanagib3@gmail.com>
2018-09-08 23:05:28 +0200

    added.

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,5 @@
 //warpper
+
 $(document).ready(function(){
   $(".menu-icon").on("click",function(){
     $("nav ul").toggleClass("showing")
@@ -22,4 +23,75 @@ $("#imgModal1").on("show", function () {
   $("body").addClass("modal-open");
 }).on("hidden", function () {
   $("body").removeClass("modal-open")
+});
+//login Register button 2 in one page
+$(document).ready(function(){
+  $("#signup-frm-btn").click(function(){
+
+    $(".login").hide('slow');
+    $(".signup").show('slow');
+    $("#signup-frm-btn").css('color','dimgray');
+
+    $("#login-frm-btn").css('color','black');
+
+
+
+  });
+  $("#login-frm-btn").click(function(){
+    $(".signup").hide('slow');
+    $(".login").show('slow');
+
+    $("#signup-frm-btn").css('color','black');
+    $("#login-frm-btn").css('color','dimgray');
+
+  })
+
+});
+
+//login function
+
+$(document).ready(function(){
+
+var userName = $('#userName').val();
+
+  $("#login").click(function(){
+    if ($('#passWord').val().length<8) {
+
+      $('.usrnmpw-error').fadeIn(100);
+
+if ($.trim(userName)==="") {
+
+  $('.usrnmpw-error').fadeIn(100);
+
+}
+}
+else {
+    $('.usrnmpw-error').hide();
+    if ($(".admin").is(':checked')) {
+        window.location.href = "admin.html";
+
+    }
+    else if ($(".user").is(':checked')) {
+        window.location.href = "user.html"
+
+    }
+    else {
+        $(".checkbox-error").show();
+
+    }
+}
+
+
+  })
+});
+//checkbox one box checked only
+$(document).ready(function () {
+    $('.check').click(function () {
+        $('.check').not(this).prop('checked', false);
+
+    })
+});
+//submit refresh sol.
+$("#loginForm").submit(function(e) {
+    e.preventDefault();
 });
